a6cb4959cb903e0af913d9946bab3b5fb391defa
johnklee <puremonkey2001@yahoo.com.tw>
2021-03-09 23:34:55 -0800

    improve utility and kaggle_titanic demo

--- kutils/analysis/__init__.py
+++ kutils/analysis/__init__.py
@@ -6,6 +6,15 @@ import matplotlib.pyplot as plt
 from functools import partial
 
 
+def corr(data, target_column, top_n=20):
+    corr_df = data.corr().abs()
+    s = corr_df.unstack()
+    so = s.sort_values(kind="quicksort", ascending=False).reset_index()
+    top_df = so[(so['level_0']!=so['level_1']) & (so['level_1'] == target_column)][:top_n]
+    top_df = top_df.rename(columns={'level_0':'feature', 'level_1':'target', 0:'corr'})
+    return top_df
+
+
 class GeneralPlotUtils:
     def __init__(self, figsize=(20,4)):
         self.set_figsize(figsize)
